517fad35c3b0fa172666ae935316268e4c114619
Damir Arh <damir.arh@gmail.com>
2021-09-11 18:28:20 +0200

    Restrict directive to img element

--- src/app/hide-missing.directive.ts
+++ src/app/hide-missing.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appHideMissing]',
+  selector: 'img[appHideMissing]',
 })
 export class HideMissingDirective {
   constructor(private el: ElementRef) {}
