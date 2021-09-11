2d99e9bd9f14fe76da9e7b39d0d295b10ba16fba
Damir Arh <damir.arh@gmail.com>
2021-09-11 17:29:29 +0200

    Broken rendering when image is missing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-missing-images';
+  public readonly images = [
+    'https://loremflickr.com/320/240/dog',
+    'https://loremflickr.co/320/240/broken',
+    'https://loremflickr.com/320/240/cat',
+  ];
 }
